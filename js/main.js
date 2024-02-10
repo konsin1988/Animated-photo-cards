5aadbe9c23a8a1d9b44b74019043c9bd589d23da
konsin1988 <konsin1988@gmail.com>
2024-02-10 14:07:06 +0300

    Last changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const card = document.getElementsByClassName("card");
-
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
